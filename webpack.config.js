cc1acd749040addfdbe8bd4c1721d9a5a5899aab
Victor <vchweya@avicdesign.com>
2021-03-23 15:08:20 +0300

    configured html-webpack-plugin;

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -20,5 +21,11 @@ module.exports = {
                 ],
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "index.html"
+        })
+    ]
 }
